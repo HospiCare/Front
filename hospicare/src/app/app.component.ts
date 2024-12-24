dff64f98ef958a108b180c3604e93aa99841ddf0
mohamed <m.rebiai@esi-sba.dz>
2024-12-24 15:00:43 +0100

    about us first look

--- hospicare/src/app/app.component.ts
+++ hospicare/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent],
+  imports: [NavbarComponent, HeroComponent, FooterComponent, AboutUsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'hospicare';
--- hospicare/tailwind.config.js
+++ hospicare/tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0B65B1", 
-        // secondary: #181E4B
-        secondary: "#181E4B",
+        primary: "#0B65B1", // primary: #0B65B1 
+        secondary: "#181E4B", // secondary: #181E4B
+
+        button2: "#CEE0EF", // #CEE0EF
+
+        textblue:"#094679" //text color #094679
         
         
-        // // Add your custom color
+        // Add your custom color
       },
     },
   },
