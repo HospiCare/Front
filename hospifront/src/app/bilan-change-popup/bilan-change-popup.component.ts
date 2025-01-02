8c4473530a23483db8eccf8a4073105e445af2ed
mohamed <m.rebiai@esi-sba.dz>
2025-01-02 23:30:03 +0100

    still lab

--- hospifront/src/app/bilan-change-popup/bilan-change-popup.component.ts
+++ hospifront/src/app/bilan-change-popup/bilan-change-popup.component.ts
@@ -1,30 +1,28 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { BilanBio } from '../bilan-bio';
-import { BilanRadio } from '../bilan-radio';
 import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common'; // Import CommonModule
-
 
 @Component({
   selector: 'app-bilan-change-popup',
   templateUrl: './bilan-change-popup.component.html',
-  imports: [FormsModule,CommonModule], // Include FormsModule here
-
+  imports: [FormsModule, CommonModule], // Include FormsModule here
   styleUrls: ['./bilan-change-popup.component.css']
 })
 export class BilanChangePopupComponent {
-  @Input() isVisible: boolean = true;
+  @Input() isVisible: boolean = false;
   @Input() bilanType: 'biologique' | 'radiologique' = 'biologique';
-  @Input() bilanData: BilanBio | BilanRadio | undefined | null;
+  @Input() bilanData: any;
   @Output() close = new EventEmitter<void>();
-  @Output() save = new EventEmitter<string[]>(); // Emits the updated test results
 
-  editableTestResults: string[] = [];
+  editableTestResults: { name: string, result: string }[] = [];
 
   ngOnChanges() {
     if (this.bilanData && this.isBilanBio(this.bilanData)) {
-      // Initialize editable test results
-      this.editableTestResults = this.bilanData.tests.map(test => test.toString());
+      // Initialize editable test results with name and result
+      this.editableTestResults = this.bilanData.tests.map((test: { name: any; result: any; }) => ({
+        name: test.name,
+        result: test.result
+      }));
     }
   }
 
@@ -34,27 +32,12 @@ export class BilanChangePopupComponent {
     }
     return [];
   }
-  get CompteRendu() {
-    if (this.bilanData && this.isBilanRadio(this.bilanData)) {
-      return this.bilanData.compteRendu;
-    }
-    return undefined;
-  }
 
-  private isBilanBio(bilan: BilanBio | BilanRadio): bilan is BilanBio {
+  private isBilanBio(bilan: any): boolean {
     return 'tests' in bilan;
-  }
-  private isBilanRadio(bilan: BilanBio | BilanRadio): bilan is BilanRadio {
-    return 'compteRendu' in bilan;
   }
 
   onClose() {
     this.close.emit();
-  }
-
-  onSave() {
-    // Emit the updated test results
-    this.save.emit(this.editableTestResults);
-    this.onClose(); // Optionally close the popup after saving
   }
 }
