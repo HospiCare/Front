5c8c184b6077a35d050fde0ee3a8f4bef27daca0
mohamed <m.rebiai@esi-sba.dz>
2025-01-02 23:51:55 +0100

    radio1

---
+++ hospifront/src/app/radio-dash/radio-dash.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-dash',
+  imports: [],
+  templateUrl: './radio-dash.component.html',
+  styleUrl: './radio-dash.component.css'
+})
+export class RadioDashComponent {
+
+}
